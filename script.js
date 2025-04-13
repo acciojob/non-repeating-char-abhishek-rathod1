07aac931b65c5ef722aae80b74e2f3300043830c
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-13 21:44:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	
+	const charCount = {}
+
+	//first pass : count each character
+	for(let char of str){
+		charCount[char] = (charCount[char] || 0) + 1;
+	}
+
+	//second pass: find character with count 1
+	for(let char of str){
+		if(charCount[char]=== 1){
+			return char;
+		}
+			
+	}
+	return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
